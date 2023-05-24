1153931e4abdb88b5395fdf976ead3058d467558
Matt Freeman <m6freeman@tuta.io>
2023-05-23 21:20:29 -0400

    adding nvim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,16 @@
+require 'user.options'
+require 'user.keymaps'
+require 'user.plugins'
+require 'user.colorscheme'
+require 'user.cmp'                  -- Autocompletion engine and API sources
+require 'user.lsp'                  -- Language Server Protocol
+require 'user.telescope'            -- Fuzzy Finder through lists
+require 'user.treesitter'           -- Syntax highlighting
+require 'user.treesitter-context'   -- Sticky function scope
+require 'user.autopairs'            -- Auto completion for paired symbols
+require 'user.gitsigns'             -- Graphical symbols for git
+require 'user.nvim-tree'            -- Better NerdTree/*Explore
+require 'user.bufferline'           -- Display buffer names like "tabs"
+require 'user.lualine'              -- Aesthetics for Status Line
+
+
