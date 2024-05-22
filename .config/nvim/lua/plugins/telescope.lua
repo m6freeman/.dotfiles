4526b3760b4e246da07dc5c327930572093bfea4
Matt Freeman <m6freeman@tuta.io>
2024-05-21 22:44:20 -0400

    changed harpoon list chord

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -71,7 +71,7 @@ return {
                 })
                 :find()
         end
-        vim.keymap.set("n", "<C-e>", function()
+        vim.keymap.set("n", "<leader>hl", function()
             toggle_telescope(harpoon:list())
         end, { desc = "Open harpoon window" })
     end,
