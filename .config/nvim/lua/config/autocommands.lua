b57e534dbbe218d139c9cd3c70d14b3737f689e1
Matt Freeman <m6freeman@tuta.io>
2024-08-24 18:00:37 -0400

    fixing crazy sh tabstop values

--- .config/nvim/lua/config/autocommands.lua
+++ .config/nvim/lua/config/autocommands.lua
@@ -1,7 +1,18 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "sh",
+    callback = function()
+        print("Setting tab options for sh files")
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.expandtab = true
+    end,
 })
--- dev/scripts/sh/tmux_new_session.sh
+++ dev/scripts/sh/tmux_new_session.sh
@@ -2,11 +2,11 @@
 
 if [ -n "$TMUX" ]; then
 
-                        pane_current_path="$(tmux display -p "#{pane_current_path}")"
-                        session_name="$(echo "$pane_current_path" | sed 's/.*\///g')"
+    pane_current_path="$(tmux display -p "#{pane_current_path}")"
+    session_name="$(echo "$pane_current_path" | sed 's/.*\///g')"
 
-                        if ! tmux has-session -t "$session_name"; then
-                                                tmux new -s "$session_name" -c "$pane_current_path" -d
-                        fi
-                        tmux switch-client -t "$session_name"
+    if ! tmux has-session -t "$session_name"; then
+        tmux new -s "$session_name" -c "$pane_current_path" -d
+    fi
+    tmux switch-client -t "$session_name"
 fi
