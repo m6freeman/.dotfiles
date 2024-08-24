13e18b18243a79dcac98cca9d5c365a0791c2886
Matt Freeman <m6freeman@tuta.io>
2024-08-24 18:31:00 -0400

    fixed reversal of %"; tmux_new_session.sh can now be called in or out of session

--- dev/scripts/sh/tmux_new_session.sh
+++ dev/scripts/sh/tmux_new_session.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+pane_current_path="$(tmux display -p "#{pane_current_path}")"
+session_name="$(basename "$pane_current_path" | sed 's/\./_/g')"
+
+if ! tmux has-session -t "$session_name"; then
+    tmux new -s "$session_name" -c "$pane_current_path" -d
+fi
+
 if [ -n "$TMUX" ]; then
-
-    pane_current_path="$(tmux display -p "#{pane_current_path}")"
-    session_name="$(basename "$pane_current_path" | sed 's/\./_/g')"
-
-    if ! tmux has-session -t "$session_name"; then
-        tmux new -s "$session_name" -c "$pane_current_path" -d
-    fi
     tmux switch-client -t "$session_name"
+else
+    tmux attach -t "$session_name"
 fi
