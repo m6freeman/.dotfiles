09e7bffea6caaa77c1db5fa1dfac5221161727be
Matt Freeman <m6freeman@tuta.io>
2024-08-24 18:08:49 -0400

    fixing hidden folder error

--- dev/scripts/sh/tmux_new_session.sh
+++ dev/scripts/sh/tmux_new_session.sh
@@ -3,7 +3,7 @@
 if [ -n "$TMUX" ]; then
 
     pane_current_path="$(tmux display -p "#{pane_current_path}")"
-    session_name="$(echo "$pane_current_path" | sed 's/.*\///g')"
+    session_name="$(basename "$pane_current_path" | sed 's/\./_/g')"
 
     if ! tmux has-session -t "$session_name"; then
         tmux new -s "$session_name" -c "$pane_current_path" -d
