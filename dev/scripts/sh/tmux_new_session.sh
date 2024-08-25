b4ab3282b5cb046f195744e9a946a6893c7bfb7d
Matt Freeman <m6freeman@tuta.io>
2024-08-25 17:02:52 -0400

    activating python venv if present

--- dev/scripts/sh/tmux_new_session.sh
+++ dev/scripts/sh/tmux_new_session.sh
@@ -4,6 +4,16 @@ pane_current_path="$(tmux display -p "#{pane_current_path}")"
 session_name="$(basename "$pane_current_path" | sed 's/\./_/g')"
 
 if ! tmux has-session -t "$session_name"; then
+    if [[ -z "$VIRTUAL_ENV" ]]; then
+        if [[ -d "$pane_current_path/.venv" ]]; then
+            source "$pane_current_path/.venv/bin/activate"
+        fi
+    else
+        parentdir="$(dirname "$VIRTUAL_ENV")"
+        if [[ "$PWD"/ != "$parentdir"/* ]]; then
+            deactivate
+        fi
+    fi
     tmux new -s "$session_name" -c "$pane_current_path" -d nvim
     tmux split-window -h -c "$pane_current_path"
 fi
