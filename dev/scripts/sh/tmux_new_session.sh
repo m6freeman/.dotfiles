8306a45ffca485da2cb2b8fc8cf1252d57ce1c2e
Matt Freeman <m6freeman@tuta.io>
2024-08-25 16:41:03 -0400

    remain in tmux sessions on session exit;

--- dev/scripts/sh/tmux_new_session.sh
+++ dev/scripts/sh/tmux_new_session.sh
@@ -4,7 +4,8 @@ pane_current_path="$(tmux display -p "#{pane_current_path}")"
 session_name="$(basename "$pane_current_path" | sed 's/\./_/g')"
 
 if ! tmux has-session -t "$session_name"; then
-    tmux new -s "$session_name" -c "$pane_current_path" -d
+    tmux new -s "$session_name" -c "$pane_current_path" -d nvim
+    tmux split-window -h -c "$pane_current_path"
 fi
 
 if [ -n "$TMUX" ]; then
